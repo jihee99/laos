5d8ac47ec9f8a533e4705b595603f847d2252908
jihee <oosoojh31@gmail.com>
2023-08-18 09:44:30 +0900

    add generateAndDownloadExcel api

--- src/main/java/com/example/laos/controller/TankController.java
+++ src/main/java/com/example/laos/controller/TankController.java
@@ -1,10 +1,13 @@
 package com.example.laos.controller;
 
+import com.example.laos.util.GenerateAndDownloadTankInputExcel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @Slf4j
@@ -16,4 +19,15 @@ public class TankController {
         ModelAndView mav = new ModelAndView("tank");
         return mav;
     }
+
+    @GetMapping("/generateAndDownloadExcel")
+    public void generateAndDownloadExcel(HttpServletResponse response) {
+        GenerateAndDownloadTankInputExcel.generateAndDownloadExcel(response);
+    }
+
+
+
+
+
+
 }
